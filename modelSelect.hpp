004ba55f60a99e133b2604787a98c7a407343233
Adrian Yip <yipadrian1119@gmail.com>
2026-06-02 16:28:15 -0400

    feat: setting up shaderparams in modelSelect

--- modelSelect.hpp
+++ modelSelect.hpp
@@ -32,11 +32,12 @@ class ModelSelect {
             currentModel = NONE_MODEL;
 
             currentShader = nullptr;
+            outlineShader = nullptr;
             currentStyle = NONE_STYLE;
         }
 
         void selectModel(ModelType model, Style shaderStyle, ShaderParams& params, enginemath::Mat4 projection,
-        enginemath::Mat4 view) {
+        enginemath::Mat4 view, enginemath::Vec3 cameraPos) {
         
             switch (model) {
                 case (GENSHIN): {
@@ -46,7 +47,7 @@ class ModelSelect {
                         currentModel = model;
                     }
                     enginemath::Mat4 genshinModelMat = enginemath::Mat4::translationM(0.0f, -1.0f, 0.0f);
-                    selectStyle(shaderStyle, params, projection, view, genshinModelMat);
+                    selectStyle(shaderStyle, params, projection, view, cameraPos, genshinModelMat);
                     break;
                 }
                 case (SAMUS): {
@@ -56,7 +57,7 @@ class ModelSelect {
                         currentModel = model;
                     }
                     enginemath::Mat4 samusModelMat = enginemath::Mat4::translationM(0.0f, -1.0f, 0.0f) * enginemath::Mat4::scaleM(0.1f, 0.1f, 0.1f);
-                    selectStyle(shaderStyle, params, projection, view, samusModelMat);
+                    selectStyle(shaderStyle, params, projection, view, cameraPos, samusModelMat);
                     break;
                 }
                 case (TANGROWTH): {
@@ -66,7 +67,7 @@ class ModelSelect {
                         currentModel = model;
                     }
                     enginemath::Mat4 tanModelMat = enginemath::Mat4::translationM(0.0f, -1.0f, 0.0f) * enginemath::Mat4::scaleM(0.1f, 0.1f, 0.1f);
-                    selectStyle(shaderStyle, params, projection, view, tanModelMat);
+                    selectStyle(shaderStyle, params, projection, view, cameraPos, tanModelMat);
                     break;
                 }
 
@@ -80,10 +81,11 @@ class ModelSelect {
         ModelType currentModel;
 
         Shader* currentShader;
+        Shader* outlineShader;
         Style currentStyle;
 
         void selectStyle(Style shaderStyle, ShaderParams& params, enginemath::Mat4 projection,
-        enginemath::Mat4 view, enginemath::Mat4 modelMatrix) {
+        enginemath::Mat4 view, enginemath::Vec3 cameraPos, enginemath::Mat4 modelMatrix) {
             switch (shaderStyle) {
                 case (CARTOON): {
                     if (currentStyle != shaderStyle || currentShader == nullptr) {
@@ -91,6 +93,7 @@ class ModelSelect {
                         currentShader = new Shader("shaders/Stylized Shaders/toon/toon.vert", "shaders/Stylized Shaders/toon/toon.frag");
                         currentStyle = shaderStyle;
                     }
+
                     break;
                 }
                 case (ANIME): {
@@ -99,6 +102,33 @@ class ModelSelect {
                         currentShader = new Shader("shaders/Stylized Shaders/anime/anime.vert", "shaders/Stylized Shaders/anime/anime.frag");
                         currentStyle = shaderStyle;
                     }
+
+                    // Set up uniforms
+                    currentShader->setMat4("projection", projection);
+                    currentShader->setMat4("view", view);
+                    currentShader->setMat4("model", modelMatrix);
+                    currentShader->setVec3("uCameraPos", cameraPos);
+
+                    currentShader->setVec3("uDirectionalLight", params.dirLight);
+
+                    currentShader->setVec4("uSpecular", params.specular);
+                    currentShader->setVec4("uRimColor", params.rimColor);
+                    currentShader->setFloat("uGlossiness", params.glossiness);
+
+                    // Render the model (1st pass)
+                    shaderWithoutOutline(*currentShader, *currentlyLoaded);
+
+                    if (params.outline) {
+                        if (outlineShader == nullptr) {
+                            outlineShader = new Shader("shaders/Stylized Shaders/anime/outline.vert", "shaders/Stylized Shaders/anime/outline.frag");
+                        }
+                        outlineShader->setMat4("projection", projection);
+                        outlineShader->setMat4("view", view);
+                        outlineShader->setMat4("model", modelMatrix);
+                        outlineShader->setFloat("uOutlineWidth", params.outlineWidth);
+                        shaderWithOutline(*outlineShader, *currentlyLoaded);
+                    }
+
                     break;
                 }
             };
@@ -117,6 +147,8 @@ class ModelSelect {
 
             glCullFace(GL_BACK);
         }
+
+       
 };
 
 #endif
